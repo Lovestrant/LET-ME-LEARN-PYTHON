17eac8582382b5af86837498b2214f50a430d5bf
Lovestrant Kemboi <71724387+Lovestrant@users.noreply.github.com>
2022-05-23 17:59:39 +0300

    first commit

---
+++ index.py
@@ -0,0 +1,47 @@
+# print ("Hello World!")
+
+# age = input("Enter Age\n")
+# username = input("Enter Username\n")
+
+# print(f'hello, your age: {age} and username is {username}')
+
+# Theage = int(age)
+# sum = str(Theage)+"10"
+
+# print(f"The sum is: {sum}")
+
+#Python Arrays
+
+# thearray = ["hello", "welcome", 20]
+
+# print(f"The first element of Array: {thearray[0]}")
+# print(f"The Last element of Array: {thearray[2]}")
+
+# print(f"The whole Array: {thearray}")
+
+# addFinalValue = input("Enter value to add to array\n")
+
+# thearray.append(addFinalValue)
+# print(f"The whole Array: {thearray}")
+
+# popItemFromArray = input("Enter index to pop\n")
+
+# thearray.pop()
+
+# print(f"The whole Array: {thearray}")
+
+# #Tuples, lists,dictionaries, 
+
+firstTuple = (1,3,4,6)
+
+myList = [[1,2,3], [3,4,5]]
+
+print(myList[0][2])
+
+firstTuple.append("Hello")
+
+print(f"Tuple is: {firstTuple}")
+
+
+
+
